28612862b1a5813acd1df69277061dcfdb28ed99
bmxcmx <cadenwhitfield86@gmail.com>
2026-01-16 17:48:39 -0500

    Revert "Turret Targets the center of the hub"
    This reverts commit 44c191127cc7d225c14bc7b45cc2f3988e7cac2e.

--- src/main/java/frc/robot/bobot_state2/BobotState.java
+++ src/main/java/frc/robot/bobot_state2/BobotState.java
@@ -1,7 +1,6 @@
 package frc.robot.bobot_state2;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
