ab8bc5019d7a05830421b36fac991f2980bf863b
bmxcmx <cadenwhitfield86@gmail.com>
2026-03-05 16:55:39 -0500

    removing failed testing things

--- src/main/java/frc/robot/util/ShooterHelper.java
+++ src/main/java/frc/robot/util/ShooterHelper.java
@@ -5,76 +5,10 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.Constants;
 import frc.robot.Constants.TurretConstants;
-import java.util.Optional;
 
 public class ShooterHelper {
-  public final class TrasnitionTime {
-
-    public static boolean isHubActive() {
-      Optional<Alliance> alliance = DriverStation.getAlliance();
-      // If we have no alliance, we cannot be enabled, therefore no hub.
-      if (alliance.isEmpty()) {
-        return false;
-      }
-      // Hub is always enabled in autonomous.
-      if (DriverStation.isAutonomousEnabled()) {
-        return true;
-      }
-      // At this point, if we're not teleop enabled, there is no hub.
-      if (!DriverStation.isTeleopEnabled()) {
-        return false;
-      }
-
-      // We're teleop enabled, compute.
-      double matchTime = DriverStation.getMatchTime();
-      String gameData = DriverStation.getGameSpecificMessage();
-      // If we have no game data, we cannot compute, assume hub is active, as its likely early in
-      // teleop.
-      if (gameData.isEmpty()) {
-        return true;
-      }
-      boolean redInactiveFirst = false;
-      switch (gameData.charAt(0)) {
-        case 'R' -> redInactiveFirst = true;
-        case 'B' -> redInactiveFirst = false;
-        default -> {
-          // If we have invalid game data, assume hub is active.
-          return true;
-        }
-      }
-
-      // Shift was is active for blue if red won auto, or red if blue won auto.
-      boolean shift1Active =
-          switch (alliance.get()) {
-            case Red -> !redInactiveFirst;
-            case Blue -> redInactiveFirst;
-          };
-
-      if (matchTime > 130) {
-        // Transition shift, hub is active.
-        return true;
-      } else if (matchTime > 105) {
-        // Shift 1
-        return shift1Active;
-      } else if (matchTime > 80) {
-        // Shift 2
-        return !shift1Active;
-      } else if (matchTime > 55) {
-        // Shift 3
-        return shift1Active;
-      } else if (matchTime > 30) {
-        // Shift 4
-        return !shift1Active;
-      } else {
-        // End game, hub always active.
-        return true;
-      }
-    }
-  }
 
   public final class TurretAim {
 
